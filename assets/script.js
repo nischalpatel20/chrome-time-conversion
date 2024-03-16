6acf3ccee9c5ff9fbeca3e63191dbeeb39c191df
nischalpatel20 <nischal.patel@razorpay.com>
2024-03-17 01:19:24 +0530

    handle commas & remove button

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
 			mainFunction();
 		}
 	});
+	$("#timestamp").on('input', function(){
+        toDate(); // Convert timestamp to date whenever the input changes
+    });
+
+    // Listen for input events on the date-format field to convert it to a timestamp
+    $("#date-format").on('input', function(){
+        toTimestamp(); // Convert date to timestamp whenever the input changes
+    });
 
 	$("input").focusin(function(){
 		var type = $(this).attr("id");
@@ -42,7 +50,7 @@ $(document).ready(function(){
 
 function mainFunction(){
 	var mode = parseInt($("#mode").val());
-
+	
 	if(mode == 0)
 		toDate();
 	else
@@ -54,19 +62,26 @@ function changeModeValue(new_value){
 }
 
 function toTimestamp(){
-	var myDate = document.getElementById('date-format').value;
-	myDate = myDate.split("-");
-
-	var newDate = myDate[1] + "/" + myDate[0] + "/" + myDate[2];
-	var new_time = new Date(newDate).getTime();
 	
-	$("#timestamp").val(new_time/1000);
+	var myDate = document.getElementById('date-format').value;
+    // Replace all commas with dashes and remove any characters that are not digits or dashes
+    myDate = myDate.replace(/,/g, "-").replace(/[^\d-]/g, "");
+    
+    var newDate = myDate.split("-");
+    var newDateFormatted = newDate[1] + "/" + newDate[0] + "/" + newDate[2];
+    var new_time = new Date(newDateFormatted).getTime();
+    
+    $("#timestamp").val(new_time / 1000);
 }
-
 function toDate(){
-	var timestamp = document.getElementById('timestamp').value;
-	date = new Date( + timestamp * 1000);
-	$("#date-format").val(date);
+    var timestamp = document.getElementById('timestamp').value.replace(/,/g, '');
+    if (timestamp.trim() === '') {
+        $("#date-format").val(''); // Clear the date field if timestamp is empty
+        return;
+    }
+    
+    var date = new Date(+timestamp * 1000);
+    $("#date-format").val(date);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
